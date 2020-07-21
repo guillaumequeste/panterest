d2f1b4afd4448970aa10c16567fa080ef3ce7246
Guillaume Queste <guillaume.queste@laposte.net>
2020-07-21 10:22:40 +0200

    add image_name field to pins table

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -44,6 +44,11 @@ class Pin
      */
     private $updatedAt;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -108,5 +113,17 @@ class Pin
         }
         
         $this->setUpdatedAt(new \DateTime);
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
     }
 }
